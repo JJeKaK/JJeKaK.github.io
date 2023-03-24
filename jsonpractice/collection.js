6f9ff58f186fb8f95635ada8081bdd3f51a761df
JJeKaK <114003845+JJeKaK@users.noreply.github.com>
2023-03-24 13:37:13 -0400

    Update collection.js

--- jsonpractice/collection.js
+++ jsonpractice/collection.js
@@ -1,22 +1,17 @@
-// Function to render your items
 const renderItems = (collection) => {
-	// The `ul` where the items will be inserted
-	const collectionList = document.getElementById('collection')
-
-	// Loop through each item in the collection array
+    const collectionList = document.getElementById('collection')
+    
 	collection.forEach((item) => {
 		const listItem = document.createElement('li') // Make the `li`
 
-
 		// You can make each element inside of that…
-		const itemTitle = document.createElement('h2') // Make an `h2`
-		itemTitle.innerHTML = item.address // Put the JSON title inside
+		const address = document.createElement('h2') // Make an `h2`
+		address.innerHTML = item.address // Put the JSON title inside
 		listItem.appendChild(address) // And add it to the `li`!
 
-		const itemImage = document.createElement('iframe') // And an image
-		itemImage.src = item.iframe // Set the `src` attribute from the JSON
-		listItem.appendChild(itemImage) // And add that too
-
+		const gglImage = document.createElement('iframe') // And an image
+		gglImage.src = item.iframe // Set the `src` attribute from the JSON
+		listItem.appendChild(gglImage) // And add that too
 
 		// // This can get annoying, so we can use “template literals” instead
 		// const itemDetails =
@@ -29,10 +24,10 @@ const renderItems = (collection) => {
 		// 	`
 		// listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
 
-		// You can build logic from your data, too
-		if (!item.alsoWriter) { // If this is `false`
-			listItem.classList.add('faded') // Add this class to the whole `li`
-		}
+		// // You can build logic from your data, too
+		// if (!item.alsoWriter) { // If this is `false`
+		// 	listItem.classList.add('faded') // Add this class to the whole `li`
+		// }
 
 		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
 	})
@@ -40,8 +35,8 @@ const renderItems = (collection) => {
 
 
 
-// Fetch gets your JSON file… 
-fetch("/jsonpractice/collection.json")
+fetch('/jsonpractice/collection.json')
 	.then((response) => response.json())
-	.then((data) => console.log(data)
-    );
+	.then((collection) => {
+        renderItems(collection)
+    });
