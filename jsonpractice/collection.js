9aa599fdd8355b105dfae715d52790820d7ae1cf
JJeKaK <114003845+JJeKaK@users.noreply.github.com>
2023-03-24 13:49:58 -0400

    Update collection.js

--- jsonpractice/collection.js
+++ jsonpractice/collection.js
@@ -2,32 +2,28 @@ const renderItems = (collection) => {
     const collectionList = document.getElementById('collection')
     
 	collection.forEach((item) => {
-		const listItem = document.createElement('li') // Make the `li`
+		const listItem = document.createElement('li')
 
-		// You can make each element inside of that…
-		const address = document.createElement('h2') // Make an `h2`
-		address.innerHTML = item.address // Put the JSON title inside
-		listItem.appendChild(address) // And add it to the `li`!
+		const address = document.createElement('h2')
+		address.innerHTML = item.address
+		listItem.appendChild(address)
 
-		const gglImage = document.createElement('iframe') // And an image
-		gglImage.src = item.iframe // Set the `src` attribute from the JSON
-		listItem.appendChild(gglImage) // And add that too
+		const gglImage = document.createElement('iframe')
+		gglImage.src = item.iframe
+		listItem.appendChild(gglImage)
 
-		// // This can get annoying, so we can use “template literals” instead
-		// const itemDetails =
-		// 	`
-		// 		<p>Distance from guess: <time>${item.miles}</time> Miles</p>
-		// 		<p>Points: <em>${item.points}</em></p>
-		// 		<a href="${item.link}">
-		// 			<p>${item.number} / 5 →</p>
-		// 		</a>
-		// 	`
-		// listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
-
-		// // You can build logic from your data, too
-		// if (!item.alsoWriter) { // If this is `false`
-		// 	listItem.classList.add('faded') // Add this class to the whole `li`
-		// }
+		// template literals
+		const itemDetails =
+			`
+                <p>Monthly Fee: ${item.monthlyFee}</p>
+				<p>Bathroom(s): ${item.bathrooms}</p>
+                <p>It's ${item.school} from The New School</p>
+				<a href="${item.imdLink}">
+					<p>Google Map →</p>
+				</a>
+                <br>
+            `
+		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
 
 		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
 	})
