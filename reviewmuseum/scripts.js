c2ae2009899ef13d474671716931ea837752bec8
JJeKaK <114003845+JJeKaK@users.noreply.github.com>
2023-04-04 06:46:51 -0400

    javascript

---
+++ reviewmuseum/scripts.js
@@ -0,0 +1,41 @@
+const renderItems = (collection) => {
+    const collectionList = document.getElementById('collection')
+    
+	collection.forEach((item) => {
+		const listItem = document.createElement('li')
+
+		const name = document.createElement('h2')
+		name.innerHTML = item.name
+		listItem.appendChild(name)
+
+		// const gglImage = document.createElement('iframe')
+		// gglImage.src = item.iframe
+		// listItem.appendChild(gglImage)
+
+		const itemDetails =
+			`
+                <p>Released date: ${item.released}</p>
+				<p>Genre: ${item.genre}</p>
+                <p>Tomatometer: ${item.Tomatometer}</p>
+                <p>AudienceScore: ${item.AudienceScore}</p>
+                <p>${item.review1}</p>
+                <p>${item.review1trans}</p>
+                <p>${item.review2}</p>
+                <p>${item.review2trans}</p>
+                <p>${item.review3}</p>
+                <p>${item.review3trans}</p>
+                <br>
+            `
+		listItem.insertAdjacentHTML('beforeend', itemDetails)
+
+		collectionList.appendChild(listItem)
+	})
+}
+
+
+
+fetch('/reviewmuseum/collection.json')
+	.then((response) => response.json())
+	.then((collection) => {
+        renderItems(collection)
+    });
