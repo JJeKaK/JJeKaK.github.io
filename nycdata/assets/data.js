c0d4fae785eccc5266f1ef5ba56c428dd9db925f
JJeKaK <114003845+JJeKaK@users.noreply.github.com>
2023-04-21 18:06:29 -0400

    fetched our data, pulled into entire thing

--- nycdata/assets/data.js
+++ nycdata/assets/data.js
@@ -1,1 +1,7 @@
-const url = 'https://data.cityofnewyork.us/resource/3nja-bsch.json'
+const dataUrl = 'https://data.cityofnewyork.us/resource/3nja-bsch.json'
+
+fetch(dataUrl + '?$limit=100')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
