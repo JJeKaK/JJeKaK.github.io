d60a4382e04afdda6e6ad2280208af63ed9be327
JJeKaK <114003845+JJeKaK@users.noreply.github.com>
2023-05-05 10:48:49 -0400

    separate previous js and d3 experiment

--- nycdata/assets/d3.js
+++ nycdata/assets/d3.js
@@ -2,41 +2,6 @@ const dataUrl = 'https://data.cityofnewyork.us/resource/3nja-bsch.json'
 
 let localData = []
 
-// const network = (programs) => {
-//   let counteeCullenNetwork = 0
-//   let sewardParkNetwork = 0
-//   let specialUnit = 0
-
-//   programs.forEach(program => {
-//       if (program.network == 'Countee Cullen Network') {
-//           counteeCullenNetwork++
-//       }
-//       else if (program.network == 'Seward Park Network') {
-//           sewardParkNetwork++
-//       }
-//       else if (program.network == 'Special Unit') {
-//           specialUnit++
-//       }
-//   });
-
-//   console.log('Countee Cullen Network: ' + counteeCullenNetwork)
-//   console.log('Seward Park Network: ' + sewardParkNetwork)
-//   console.log('Special Unit: ' + specialUnit)
-
-//   const networkList = document.getElementsById('networkList')
-//   programs.forEach(item => {
-//       const listProgram = document.createElement('li')
-//       const programDetails =
-//       `
-//       <p>${item.counteeCullenNetwork}</p>
-//       <p>${item.data.sewardParkNetwork}</p>
-//       <p>${item.data.specialUnit}</p>
-//       `
-//       listProgram.insertAdjacentHTML('beforeend', programDetails)
-//       networkList.appendChild(listProgram)
-//   });
-// }
-
 fetch(dataUrl)
     .then(response => response.json())
     .then(data => {
