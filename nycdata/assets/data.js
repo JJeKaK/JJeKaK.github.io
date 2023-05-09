8145d5591bf83c32dab484b1983f8d4eb80c8135
JJeKaK <114003845+JJeKaK@users.noreply.github.com>
2023-05-09 07:53:17 -0400

    nycdata

--- nycdata/assets/data.js
+++ nycdata/assets/data.js
@@ -21,22 +21,45 @@ const network = (programs) => {
 
   console.log('Countee Cullen Network: ' + counteeCullenNetwork)
   console.log('Seward Park Network: ' + sewardParkNetwork)
-
-  const networkList = document.getElementsById('networkList')
-  programs.forEach(item => {
-      const listProgram = document.createElement('li')
-      const programDetails =
-      `
-      <p>${item.counteeCullenNetwork}</p>
-      <p>${item.data.sewardParkNetwork}</p>
-      `
-      listProgram.insertAdjacentHTML('beforeend', programDetails)
-      networkList.appendChild(listProgram)
-  });
 }
+
+// const aProgramz = (aPrograms) => {
+//     let adult_program = 0
+//     let young_adult_program = 0
+//     let juvenile_program = 0
+//     let outreach_services_program = 0
+
+//     aPrograms.forEach(aProgram => {
+//         if (aProgram.aProgramz == 'Adult Programs') {
+//             adult_program++
+//         }
+//         else if (aProgram.aProgramz == 'Young Adult Programs') {
+//             young_adult_program++
+//         }
+//         else if (aProgram.aProgramz == 'Juvenile Programs') {
+//             juvenile_program++
+//         }
+//         else if (aProgram.aProgramz == 'Outreach Services Programs') {
+//             outreach_services_program++
+//         }
+//     });
+
+//     console.log('Adult Programs: ' + adult_program)
+//     console.log('Young Adult Programs: ' + young_adult_program)
+//     console.log('Juvenile Programs: ' + juvenile_program)
+//     console.log('Outreach Services Programs: ' + outreach_services_program)
+// }
+
+
 
 fetch(dataUrl)
     .then(response => response.json())
     .then(data => {
         network(data)
 })
+
+// fetch(dataUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//         aProgramz(data)
+// })
